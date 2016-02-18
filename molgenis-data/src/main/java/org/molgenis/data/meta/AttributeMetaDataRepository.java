783d1d728e42103ca0e2ba501eeb3a35e4a8e01c
dennishendriksen <d.hendriksen@umcg.nl>
2016-02-18 15:28:37 +0100

    Replace duplicate code with reference to MetaUtils

--- molgenis-data/src/main/java/org/molgenis/data/meta/AttributeMetaDataRepository.java
+++ molgenis-data/src/main/java/org/molgenis/data/meta/AttributeMetaDataRepository.java
@@ -3,45 +3,21 @@ package org.molgenis.data.meta;
 import static java.util.Objects.requireNonNull;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.StreamSupport.stream;
-import static org.molgenis.data.meta.AttributeMetaDataMetaData.AGGREGATEABLE;
-import static org.molgenis.data.meta.AttributeMetaDataMetaData.AUTO;
-import static org.molgenis.data.meta.AttributeMetaDataMetaData.DATA_TYPE;
-import static org.molgenis.data.meta.AttributeMetaDataMetaData.DEFAULT_VALUE;
-import static org.molgenis.data.meta.AttributeMetaDataMetaData.DESCRIPTION;
-import static org.molgenis.data.meta.AttributeMetaDataMetaData.ENUM_OPTIONS;
-import static org.molgenis.data.meta.AttributeMetaDataMetaData.EXPRESSION;
 import static org.molgenis.data.meta.AttributeMetaDataMetaData.IDENTIFIER;
-import static org.molgenis.data.meta.AttributeMetaDataMetaData.LABEL;
-import static org.molgenis.data.meta.AttributeMetaDataMetaData.NAME;
-import static org.molgenis.data.meta.AttributeMetaDataMetaData.NILLABLE;
 import static org.molgenis.data.meta.AttributeMetaDataMetaData.PARTS;
-import static org.molgenis.data.meta.AttributeMetaDataMetaData.RANGE_MAX;
-import static org.molgenis.data.meta.AttributeMetaDataMetaData.RANGE_MIN;
-import static org.molgenis.data.meta.AttributeMetaDataMetaData.READ_ONLY;
-import static org.molgenis.data.meta.AttributeMetaDataMetaData.REF_ENTITY;
-import static org.molgenis.data.meta.AttributeMetaDataMetaData.UNIQUE;
-import static org.molgenis.data.meta.AttributeMetaDataMetaData.VALIDATION_EXPRESSION;
-import static org.molgenis.data.meta.AttributeMetaDataMetaData.VISIBLE;
-import static org.molgenis.data.meta.AttributeMetaDataMetaData.VISIBLE_EXPRESSION;
 
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
-import org.molgenis.MolgenisFieldTypes;
 import org.molgenis.data.AttributeMetaData;
 import org.molgenis.data.Entity;
 import org.molgenis.data.ManageableRepositoryCollection;
-import org.molgenis.data.Range;
 import org.molgenis.data.Repository;
 import org.molgenis.data.i18n.LanguageService;
 import org.molgenis.data.support.DefaultAttributeMetaData;
-import org.molgenis.data.support.MapEntity;
 import org.molgenis.data.support.UuidGenerator;
-import org.molgenis.fieldtypes.CompoundField;
-import org.molgenis.fieldtypes.EnumField;
 
-import com.google.common.base.Joiner;
 import com.google.common.collect.Iterables;
 
 /**
@@ -111,64 +87,9 @@ class AttributeMetaDataRepository
 
 			private Entity convertToAttrEntity(AttributeMetaData attr)
 			{
-				Entity attributeMetaDataEntity = new MapEntity(META_DATA);
-				attributeMetaDataEntity.set(IDENTIFIER, uuidGenerator.generateId());
-				attributeMetaDataEntity.set(NAME, attr.getName());
-				attributeMetaDataEntity.set(DATA_TYPE, attr.getDataType());
-				attributeMetaDataEntity.set(NILLABLE, attr.isNillable());
-				attributeMetaDataEntity.set(AUTO, attr.isAuto());
-				attributeMetaDataEntity.set(VISIBLE, attr.isVisible());
-				attributeMetaDataEntity.set(LABEL, attr.getLabel());
-				attributeMetaDataEntity.set(DESCRIPTION, attr.getDescription());
-				attributeMetaDataEntity.set(AGGREGATEABLE, attr.isAggregateable());
-				attributeMetaDataEntity.set(READ_ONLY, attr.isReadonly());
-				attributeMetaDataEntity.set(UNIQUE, attr.isUnique());
-				attributeMetaDataEntity.set(EXPRESSION, attr.getExpression());
-				attributeMetaDataEntity.set(VISIBLE_EXPRESSION, attr.getVisibleExpression());
-				attributeMetaDataEntity.set(VALIDATION_EXPRESSION, attr.getValidationExpression());
-				attributeMetaDataEntity.set(DEFAULT_VALUE, attr.getDefaultValue());
-
-				if ((attr.getDataType() instanceof EnumField) && (attr.getEnumOptions() != null))
-				{
-					attributeMetaDataEntity.set(ENUM_OPTIONS, Joiner.on(',').join(attr.getEnumOptions()));
-				}
-
-				if (attr.getRange() != null)
-				{
-					attributeMetaDataEntity.set(RANGE_MIN, attr.getRange().getMin());
-					attributeMetaDataEntity.set(RANGE_MAX, attr.getRange().getMax());
-				}
-
-				if (attr.getRefEntity() != null)
-				{
-					String entityName = attr.getRefEntity().getName();
-					attributeMetaDataEntity.set(REF_ENTITY, entityName);
-				}
-
-				// recursive for compound attribute parts
-				if (attr.getDataType() instanceof CompoundField)
-				{
-					List<Entity> attrPartsEntities = convertToAttrEntities(attr.getAttributeParts());
-					repository.add(attrPartsEntities.stream());
-					attributeMetaDataEntity.set(PARTS, attrPartsEntities);
-				}
-
-				// Language attributes
-				for (String languageCode : attr.getLabelLanguageCodes())
-				{
-					String attributeName = LABEL + '-' + languageCode;
-					String label = attr.getLabel(languageCode);
-					if (label != null) attributeMetaDataEntity.set(attributeName, label);
-				}
-
-				for (String languageCode : attr.getDescriptionLanguageCodes())
-				{
-					String attributeName = DESCRIPTION + '-' + languageCode;
-					String description = attr.getDescription(languageCode);
-					if (description != null) attributeMetaDataEntity.set(attributeName, description);
-				}
-
-				return attributeMetaDataEntity;
+				Entity attrEntity = MetaUtils.toEntity(attr);
+				attrEntity.set(IDENTIFIER, uuidGenerator.generateId());
+				return attrEntity;
 			}
 		};
 	}
@@ -208,57 +129,9 @@ class AttributeMetaDataRepository
 	 * @return {@link DefaultAttributeMetaData}, with {@link DefaultAttributeMetaData#getRefEntity()} properly filled if
 	 *         needed.
 	 */
-	public DefaultAttributeMetaData toAttributeMetaData(Entity entity)
+	public AttributeMetaData toAttributeMetaData(Entity entity)
 	{
-		DefaultAttributeMetaData attributeMetaData = new DefaultAttributeMetaData(entity.getString(NAME));
-		attributeMetaData.setIdentifier(entity.getString(IDENTIFIER));
-		attributeMetaData.setDataType(MolgenisFieldTypes.getType(entity.getString(DATA_TYPE)));
-		attributeMetaData.setNillable(entity.getBoolean(NILLABLE));
-		attributeMetaData.setAuto(entity.getBoolean(AUTO));
-		attributeMetaData.setVisible(entity.getBoolean(VISIBLE));
-		attributeMetaData.setLabel(entity.getString(LABEL));
-		attributeMetaData.setDescription(entity.getString(DESCRIPTION));
-		attributeMetaData
-				.setAggregateable(entity.getBoolean(AGGREGATEABLE) == null ? false : entity.getBoolean(AGGREGATEABLE));
-		attributeMetaData.setEnumOptions(entity.getList(ENUM_OPTIONS));
-		attributeMetaData.setReadOnly(entity.getBoolean(READ_ONLY) == null ? false : entity.getBoolean(READ_ONLY));
-		attributeMetaData.setUnique(entity.getBoolean(UNIQUE) == null ? false : entity.getBoolean(UNIQUE));
-		attributeMetaData.setExpression(entity.getString(EXPRESSION));
-
-		Long rangeMin = entity.getLong(RANGE_MIN);
-		Long rangeMax = entity.getLong(RANGE_MAX);
-		if ((rangeMin != null) || (rangeMax != null))
-		{
-			attributeMetaData.setRange(new Range(rangeMin, rangeMax));
-		}
-		if (entity.get(REF_ENTITY) != null)
-		{
-			final String refEntityName = entity.getString(REF_ENTITY);
-			attributeMetaData.setRefEntity(entityMetaDataRepository.get(refEntityName));
-		}
-		Iterable<Entity> parts = entity.getEntities(PARTS);
-		if (parts != null)
-		{
-			stream(parts.spliterator(), false).map(this::toAttributeMetaData)
-					.forEach(attributeMetaData::addAttributePart);
-		}
-		attributeMetaData.setVisibleExpression(entity.getString(VISIBLE_EXPRESSION));
-		attributeMetaData.setValidationExpression(entity.getString(VALIDATION_EXPRESSION));
-		attributeMetaData.setDefaultValue(entity.getString(DEFAULT_VALUE));
-
-		// Language attributes
-		for (String languageCode : languageService.getLanguageCodes())
-		{
-			String attributeName = LABEL + '-' + languageCode;
-			String label = entity.getString(attributeName);
-			if (label != null) attributeMetaData.setLabel(languageCode, label);
-
-			attributeName = DESCRIPTION + '-' + languageCode;
-			String description = entity.getString(attributeName);
-			if (description != null) attributeMetaData.setDescription(languageCode, description);
-		}
-
-		return attributeMetaData;
+		return MetaUtils.toAttribute(entity, entityMetaDataRepository.getRepository(),
+				languageService.getLanguageCodes());
 	}
-
 }
