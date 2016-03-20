0b4842c17dc0dcf79cef76dc9e80aabdced61935
dennishendriksen <d.hendriksen@umcg.nl>
2016-03-20 15:36:15 +0100

    Transactional cache improvements

--- molgenis-data/src/main/java/org/molgenis/data/cache/TransactionEntityCache.java
+++ molgenis-data/src/main/java/org/molgenis/data/cache/TransactionEntityCache.java
@@ -1,15 +1,25 @@
 package org.molgenis.data.cache;
 
+import static java.lang.String.format;
+import static java.util.Collections.emptyList;
 import static java.util.Objects.requireNonNull;
 
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import org.molgenis.MolgenisFieldTypes.FieldTypeEnum;
+import org.molgenis.data.AttributeMetaData;
 import org.molgenis.data.Entity;
+import org.molgenis.data.support.MapEntity;
 import org.molgenis.data.transaction.MolgenisTransactionListener;
 import org.molgenis.data.transaction.MolgenisTransactionManager;
+import org.molgenis.util.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,12 +34,15 @@ public class TransactionEntityCache implements MolgenisTransactionListener
 {
 	private static final Logger LOG = LoggerFactory.getLogger(TransactionEntityCache.class);
 
-	private final Map<String, Cache<String, Entity>> transactionCache; // TODO wipe xrefs/mrefs
+	// TODO wipe xrefs/mrefs
+	// TODO do not cache entity objects, but entity object values to avoid cache corruption on entity manipulation, see
+	// hydration/dehydration methods in this class
+	private final Map<String, Cache<String, Entity>> transactionEntityCaches;
 
 	@Autowired
 	public TransactionEntityCache(MolgenisTransactionManager molgenisTransactionManager)
 	{
-		transactionCache = new HashMap<>();
+		transactionEntityCaches = new HashMap<>();
 		requireNonNull(molgenisTransactionManager).addTransactionListener(this);
 	}
 
@@ -39,7 +52,7 @@ public class TransactionEntityCache implements MolgenisTransactionListener
 		String transactionId = getTransactionId();
 		if (transactionId != null)
 		{
-			Cache<String, Entity> entityCache = transactionCache.get(transactionId);
+			Cache<String, Entity> entityCache = transactionEntityCaches.get(transactionId);
 			if (entityCache != null)
 			{
 				String key = toKey(entityName, entityId);
@@ -66,11 +79,11 @@ public class TransactionEntityCache implements MolgenisTransactionListener
 		String transactionId = getTransactionId();
 		if (transactionId != null)
 		{
-			Cache<String, Entity> entityCache = transactionCache.get(transactionId);
+			Cache<String, Entity> entityCache = transactionEntityCaches.get(transactionId);
 			if (entityCache == null)
 			{
 				entityCache = createCache();
-				transactionCache.put(transactionId, entityCache);
+				transactionEntityCaches.put(transactionId, entityCache);
 			}
 			String key = toKey(entityName, entity.getIdValue());
 			entityCache.put(key, entity);
@@ -83,7 +96,7 @@ public class TransactionEntityCache implements MolgenisTransactionListener
 		String transactionId = getTransactionId();
 		if (transactionId != null)
 		{
-			Cache<String, Entity> entityCache = transactionCache.get(transactionId);
+			Cache<String, Entity> entityCache = transactionEntityCaches.get(transactionId);
 			if (entityCache != null)
 			{
 				for (Iterator<Entry<String, Entity>> it = entityCache.asMap().entrySet().iterator(); it.hasNext();)
@@ -103,13 +116,31 @@ public class TransactionEntityCache implements MolgenisTransactionListener
 		String transactionId = getTransactionId();
 		if (transactionId != null)
 		{
-			Cache<String, Entity> entityCache = transactionCache.get(transactionId);
+			Cache<String, Entity> entityCache = transactionEntityCaches.get(transactionId);
 			if (entityCache != null)
 			{
 				String key = toKey(entityName, entityId);
 				entityCache.invalidate(key);
 			}
 		}
+	}
+
+	@Override
+	public void commitTransaction(String transactionId)
+	{
+		transactionEntityCaches.remove(transactionId);
+	}
+
+	@Override
+	public void rollbackTransaction(String transactionId)
+	{
+		transactionEntityCaches.remove(transactionId);
+	}
+
+	@Override
+	public void transactionStarted(String transactionId)
+	{
+		// no op
 	}
 
 	private Cache<String, Entity> createCache()
@@ -128,21 +159,94 @@ public class TransactionEntityCache implements MolgenisTransactionListener
 				.getResource(MolgenisTransactionManager.TRANSACTION_ID_RESOURCE_NAME);
 	}
 
-	@Override
-	public void commitTransaction(String transactionId)
+	/**
+	 * Rebuild entity from the values representing this entity.
+	 * 
+	 * @param dehydratedEntity
+	 * @return
+	 */
+	private Entity hydrateEntity(List<Pair<String, Object>> dehydratedEntity)
 	{
-		transactionCache.remove(transactionId);
+		// TODO hydrate metadata
+		MapEntity entity = new MapEntity();
+		dehydratedEntity.forEach(keyValue -> {
+			// TODO hydrate value based on metadata types
+		});
+		return entity;
 	}
 
-	@Override
-	public void rollbackTransaction(String transactionId)
+	/**
+	 * Do not store entity in the cache since it might be updated by client code, instead store the values requires to
+	 * rebuild this entity. For references to other entities only store the ids.
+	 * 
+	 * @param entity
+	 * @return
+	 */
+	private List<Pair<String, Object>> dehydrateEntity(Entity entity)
 	{
-		transactionCache.remove(transactionId);
-	}
+		// TODO dehydrate metadata
+		List<Pair<String, Object>> dehydratedEntity = new ArrayList<>();
+		for (AttributeMetaData attr : entity.getEntityMetaData().getAtomicAttributes())
+		{
+			String attrName = attr.getName();
+			FieldTypeEnum attrType = attr.getDataType().getEnumType();
 
-	@Override
-	public void transactionStarted(String transactionId)
-	{
-		// no op
+			Object value;
+			switch (attrType)
+			{
+				case CATEGORICAL:
+				case XREF:
+				case FILE:
+					Entity refEntity = entity.getEntity(attrName);
+					value = refEntity != null ? refEntity.getIdValue() : null;
+					break;
+				case CATEGORICAL_MREF:
+				case MREF:
+					Iterator<Entity> refEntitiesIt = entity.getEntities(attrName).iterator();
+					if (refEntitiesIt.hasNext())
+					{
+						List<Object> mrefValues = new ArrayList<>();
+						do
+						{
+							Entity mrefEntity = refEntitiesIt.next();
+							mrefValues.add(mrefEntity != null ? mrefEntity.getIdValue() : null);
+						}
+						while (refEntitiesIt.hasNext());
+						value = mrefValues;
+					}
+					else
+					{
+						value = emptyList();
+					}
+					break;
+				case DATE:
+					// Store timestamp since data is mutable
+					Date dateValue = entity.getDate(attrName);
+					value = dateValue != null ? dateValue.getTime() : null;
+					break;
+				case DATE_TIME:
+					// Store timestamp since data is mutable
+					Timestamp dateTimeValue = entity.getTimestamp(attrName);
+					value = dateTimeValue != null ? dateTimeValue.getTime() : null;
+					break;
+				case BOOL:
+				case COMPOUND:
+				case DECIMAL:
+				case EMAIL:
+				case ENUM:
+				case HTML:
+				case HYPERLINK:
+				case INT:
+				case LONG:
+				case SCRIPT:
+				case STRING:
+				case TEXT:
+					value = entity.get(attrName);
+				default:
+					throw new RuntimeException(format("Unknown attribute type [%s]", attrType));
+			}
+			dehydratedEntity.add(new Pair<String, Object>(attrName, value));
+		}
+		return dehydratedEntity;
 	}
 }
